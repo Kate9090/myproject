b22fe062f65ab63c2dd68f9e95dadcce2fb9291e
eremina-ekaterina <eremina@uchi.ru>
2018-10-30 12:11:02 +0300

    add files

---
+++ Gulpfile.js
@@ -0,0 +1,102 @@
+var gulp = require('gulp'),
+  watch = require('gulp-watch'),
+  preFixer = require('gulp-autoprefixer'),
+  uglify = require('gulp-uglify'),
+  sass = require('gulp-sass'),
+  pug = require('gulp-pug'),
+  sourseMaps = require('gulp-sourcemaps'),
+  rigger = require('gulp-rigger'),
+  cssMin = require('gulp-minify-css'),
+  rimRaf = require('rimraf'),
+  browserSync = require('browser-sync'),
+  reload = browserSync.reload;
+
+var path = {
+  build: {
+    html: 'public/',
+    js: 'public/js/',
+    css: 'public/css/'
+  },
+  src: {
+    html: 'dev/*.pug',
+    js: 'dev/js/maim.js',
+    style: 'dev/sass/main.scss'
+  },
+  watch: {
+    html: 'dev/template/**/*.pug',
+    js: 'dev/js/**/*.js',
+    style: 'dev/sass/**/*.scss'
+  },
+  clean: './public'
+}
+
+gulp.task("webserver", function (){
+  browserSync({
+    server: {
+      baseDir: "./public"
+    },
+    host:'localhost',
+    port: 3005,
+    tunnel: true
+  });
+});
+
+gulp.task('pug', function buildHTML() {
+  return gulp.src('dev/template/pug/**/*.pug')
+
+    .pipe(pug({
+      pretty: true
+    }))
+
+    .pipe(gulp.dest('public/'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('js:build', function (){
+  gulp.src(path.src.js)
+  .pipe(rigger())
+  .pipe(sourseMaps.init)
+  .pipe(uglify())
+  .pipe(sourseMaps.write())
+  .pipe(gulp.dest(path.build.js))
+  .pipe(reload({stream: true}));
+});
+
+gulp.task('style', function() {
+    gulp.src('dev/sass/**/*.scss')
+      .pipe(sourseMaps.init)
+      .pipe(sass().on('error', sass.logError))
+      .pipe(preFixer())
+      .pipe(cssMin())
+      .pipe(sourseMaps.write())
+      .pipe(gulp.dest('public/css/'))
+      .pipe(reload({stream: true}));
+});
+
+gulp.task('build', [
+  'pug',
+  'js:build',
+  'style'
+]);
+
+gulp.task('watch', function (){
+  watch([path.watch.js], function(ev, callback){
+    gulp.start('js:build');
+  });
+  watch([path.watch.html], function(ev, callback){
+    gulp.start('html:build');
+  });
+  watch([path.watch.style], function(ev, callback){
+    gulp.start('style:build');
+  });
+})
+
+gulp.task('clean', function(callback) {
+  rimRaf(path.clean, callback);
+});
+
+gulp.task('default', [
+  'build',
+  'webserver',
+  'watch'
+]);
