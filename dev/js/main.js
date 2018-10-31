41ff208fa0b9aa193b9e70b278206e8dcfcb9ae7
eremina-ekaterina <eremina@uchi.ru>
2018-10-31 17:04:47 +0300

    menu

--- dev/js/main.js
+++ dev/js/main.js
@@ -12,5 +12,15 @@ window.onload = function () {
         preloader.classList.add('slow');
       }, 3000);
 
+//dropping menu
+    var lists= document.querySelector('.menu__lists');
+      setTimeout(function(){
+        lists.classList.add('drop');
+      }, 3000);
 
+    if (window.pageYOffset > 20) {
+      lists.classList.remove('drop');
+      lists.classList.add('disapear');
+    }
 };
+
